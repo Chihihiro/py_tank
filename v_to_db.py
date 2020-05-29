416ff49ba7f42e2005656c8477401eb96aed09ea
Chihiro <632207812@qq.com>
2020-05-29 18:59:49 +0800

    1027

--- v_to_db.py
+++ v_to_db.py
@@ -12,30 +12,51 @@ path = os.getcwd()
 with open(path+'/v.log', 'r') as f:
     v = f.read()
 
+# x = re.sub('\n','', v)
+
+x2 = re.sub('>>>', '-', v)
+all = x2.split('>')
+
+up = []
+down = []
 
 
-downlink = re.search('name: "user>>>tank>>>traffic>>>downlink"\n  value: (.+?)\n>',v).group(1)
-uplink = re.search('name: "user>>>tank>>>traffic>>>uplink"\n  value: (.+?)\n>',v).group(1)
+for i in all:
+    print(i)
+    if 'user' not in i:
+        pass
+    else:
+        user = re.search("user-(.+?)-traffic",i).group(1)
+        value = re.search("value: (.+?)\n",i).group(1)
+        if 'downlink' in i:
+            up.append([user, value])
+        else:
+            down.append([user, value])
 
-print(downlink,uplink)
 
 
 
 today = str(datetime.today().date())
-df = pd.DataFrame([{'date': today, 'uplink': int(uplink), 'downlink': int(downlink)}])
-to_sql('tank', engine, df, type="update")
 
+df1 = pd.DataFrame(up)
+df1.columns = ['user', 'uplink']
+df2 = pd.DataFrame(down)
+df2.columns = ['user', 'downlink']
+
+df3 = pd.merge(df1, df2, how='inner')
+df3['date'] = today
+df3['uplink'] = df3['uplink'].apply(lambda x: int(x))
+df3['downlink'] = df3['downlink'].apply(lambda x: int(x))
+to_sql('tank', engine, df3, type="update")
+#
+#
+#
 import time
 time.sleep(3)
 
 now = datetime.now()
 this_week_start = str(now - timedelta(days=now.weekday()))[:19]
 
-
-# sql2 = "select d1.date, tank.uplink, tank.downlink,tank.uplink+tank.downlink as up_sum FROM ( \
-# SELECT date,max(update_time) AS time FROM `tank`  GROUP BY date ) as d1 \
-# JOIN tank ON d1.date = tank.date and d1.time =tank.update_time ORDER BY date DESC "
-#
 
 sql = f"SELECT date,SUM(downlink) AS downlink, SUM(uplink) as uplink ,user, SUM(downlink+uplink) as up_sum FROM `tank` where update_time > '{this_week_start}' GROUP BY date, `user`"
 
@@ -44,3 +65,6 @@ to_sql('tank_day', engine, df, type="update")
 
 
 os.system(f'echo > {path+ "/v.log"}')
+
+
+v = os.system("""./stat api --server=193.200.134.9:54322 StatsService.QueryStats 'pattern: "" reset: true'""")
